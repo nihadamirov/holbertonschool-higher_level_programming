e62b37aeba5e80d47d4493d73f46ea113398a406
Nihad <nihadamirov98@gmail.com>
2024-05-18 19:11:12 +0000

    prints a string in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - 32)
+            result += uppercase_char
+        else:
+            result += char
+    print(result)
