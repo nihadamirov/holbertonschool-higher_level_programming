0d34dabe42ff8e610410e3f981db10466e2a9b46
Nihad <nihadamirov98@gmail.com>
2024-05-18 21:14:07 +0000

    pycodestyle validation

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,13 +7,13 @@ For multiples of five, print Buzz instead of the number.
 For multiples of three and five, print FizzBuzz instead of the number.
 
 """
-def fizzbuzz():
-    for i in range(1, 101):
-        if ((i % 3 == 0) and (i % 5 == 0)):
-            print("FizzBuzz ", end="")
-        elif (i % 3 == 0):
-            print("Fizz ", end="")
-        elif (i % 5 == 0):
-            print("Buzz ", end="")
-        else:
-            print("{} ".format(i), end="")
+ def fizzbuzz():
+     for i in range(1, 101):
+         if ((i % 3 == 0) and (i % 5 == 0)):
+             print("FizzBuzz ", end="")
+         elif (i % 3 == 0):
+             print("Fizz ", end="")
+         elif (i % 5 == 0):
+             print("Buzz ", end="")
+         else:
+             print("{} ".format(i), end="")
