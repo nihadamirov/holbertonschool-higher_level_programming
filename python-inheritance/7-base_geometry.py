1401357408487e7d69c9ad8262a62d3698680680
Nihad <nihadamirov98@gmail.com>
2024-06-02 07:51:47 +0000

    bug fix

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-"""
-Contains BaseGeometry
-with public instance method area and integer_validation
-"""
+"""[Write a class BaseGeometry (based on 6-base_geometry.py).]"""
 
 
 class BaseGeometry:
-    """
-    Methods:
-        area(self)
-        integer_validator(self, name, value)
+    """[Base Geometry class]
     """
     def area(self):
-        """not implemented"""
+        """[Public instance method: def area(self): that raises
+     an Exception with the message area() is not implemented]
+
+        Raises:
+            Exception: [area() is not implemented]
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates input
-        Args:
-            name (str): assumed always a string
-            value (int): greater than 0
+        """[Public instance method: def integer_validator(self, name, value):
+         that validates value]
+
+        Arguments:
+            name {[str]} -- [description]
+            value {[int]} -- [description]
+
+        Raises:
+            TypeError: [name must be an integer]
+            ValueError: [name must be greater than 0]
         """
-        if not isinstance(value, int):
-            raise TypeError("{:s} must be an integer".format(name))
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
