9d1e1816fc1ded2cca50d74981b3efdea97380a3
nihadamirov <nihada@goldenpay.az>
2025-06-14 10:52:02 +0400

    update shebang

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join(chr(122 - i).lower() if i % 2 == 0 else chr(122 - i).upper() for i in range(26)), end="")
+print("{}".format("".join(chr(122 - i).lower() if i % 2 == 0 else chr(122 - i).upper() for i in range(26))), end="")
